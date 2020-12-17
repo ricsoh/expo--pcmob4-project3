5b9cdd6ee7454367c57c700486dbefe539c627d3
ricsoh <ricsoh@gmail.com>
2020-12-17 18:44:51 +0800

    add TouchableWithoutFeedback

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,13 @@
+import firebase from "../database/firebaseDB";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native";
+//import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 export default function LoginScreen({ navigation }) {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const db = firebase.firestore().collection("");
 
     function gotoChatScreen() {
         setEmail("");
@@ -13,30 +16,32 @@ export default function LoginScreen({ navigation }) {
     }
 
     return(
-        <View style={{ paddingTop: 23, flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text style={styles.label}>Login</Text>
-            <Text style={styles.labelText}>Email</Text>
-            <TextInput
-                placeholder="Enter Email"
-                style={styles.textInput}
-                value={email}
-                onChangeText={(newEmail) => setEmail(newEmail)}
-            ></TextInput>
-            <Text style={styles.labelText}>Password</Text>
-            <TextInput
-                placeholder="Enter Password"
-                secureTextEntry={true}
-                style={styles.textInput}
-                value={password}
-                onChangeText={(newPassword) => setPassword(newPassword)}
-            ></TextInput>
-            <TouchableOpacity
-                onPress={gotoChatScreen}
-                style={[styles.button, styles.submitButton]}
-                >
-                <Text style={styles.buttonText}>Log In</Text>
-            </TouchableOpacity>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={{ paddingTop: 23, flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text style={styles.label}>Login</Text>
+                <Text style={styles.labelText}>Email</Text>
+                <TextInput
+                    placeholder="Enter Email"
+                    style={styles.textInput}
+                    value={email}
+                    onChangeText={(newEmail) => setEmail(newEmail)}
+                ></TextInput>
+                <Text style={styles.labelText}>Password</Text>
+                <TextInput
+                    placeholder="Enter Password"
+                    secureTextEntry={true}
+                    style={styles.textInput}
+                    value={password}
+                    onChangeText={(newPassword) => setPassword(newPassword)}
+                ></TextInput>
+                <TouchableOpacity
+                    onPress={gotoChatScreen}
+                    style={[styles.button, styles.submitButton]}
+                    >
+                    <Text style={styles.buttonText}>Log In</Text>
+                </TouchableOpacity>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
