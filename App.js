2e8e36f40222ee3200d4c2d5a9e66796029be9ea
ricsoh <ricsoh@gmail.com>
2020-12-17 16:41:21 +0800

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Home}
+          options={{
+            headerTitle: "Chat App",
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 24,  
+            },
+            headerStyle: {
+              height: 120,
+              backgroundColor: "gray",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,5 +48,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textTitle: {
+    fontWeight: "bold",
+    color: "white",
+    textAlign: 'center',    
   },
 });
